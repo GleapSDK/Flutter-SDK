7557ba8e2b2ac7c30a1338e90aa50b6a38ae0a82
Lukas Boehler <lukas@gleap.io>
2026-07-30 09:43:21 +0200

    v16.4.2

--- README.md
+++ README.md
@@ -16,7 +16,7 @@ Checkout our [documentation](https://gleap.io/docs/flutter/) for full reference.
 
 ```dart
 dependencies:
-  gleap_sdk: "^16.4.0"
+  gleap_sdk: "^16.4.2"
 ```
 
 **Flutter v2 support**
--- ios/gleap_sdk/Package.swift
+++ ios/gleap_sdk/Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         .library(name: "gleap-sdk", targets: ["gleap_sdk"])
     ],
     dependencies: [
-        .package(url: "https://github.com/GleapSDK/Gleap-iOS-SDK.git", from: "16.4.0")
+        .package(url: "https://github.com/GleapSDK/Gleap-iOS-SDK.git", from: "16.4.2")
     ],
     targets: [
         .target(
